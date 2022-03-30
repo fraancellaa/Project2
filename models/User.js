ade4473203eeffe263a13767d34d9cf036f9ffae
Francella Aguilar <francellaaguilar@Francellas-MBP.attlocal.net>
2022-03-30 13:52:42 -0400

    modifying files

--- models/User.js
+++ models/User.js
@@ -4,10 +4,38 @@ const sequelize = require('../config/connection');
 // create our User Model
 class User extends Model {}
 User.init({
-    // table column definitions go here
+   id: {
+       type: DataTypes.INTEGER,
+       allowNull: false,
+       primaryKey: true,
+       autoIncrement: true
+   },
+   username: {
+       type: DataTypes.STRING,
+       allowNull: false,
+   }, 
+   email: {
+       type: DataTypes.STRING,
+       allowNull: false,
+       unique: true,
+       validate: {
+           isEmail: true
+       }
+   },
+   password: {
+       type: DataTypes.STRING,
+       allowNull: false,
+       validate: {
+           len: [4]
+       }
+   }
 },
 {
-    // table configuration
-})
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+});
 
 module.exports = User;
