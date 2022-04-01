bbc6e41c4bed6b529bdefb9810b859b4ef6e8e1b
CougarChan <gohan0136@gmail.com>
2022-03-31 20:23:11 -0400

    Update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const routes = require('./controllers/');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 // const helpers = require('./utils/helpers');
 
@@ -29,11 +29,11 @@ const sess = {
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
