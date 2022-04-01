5f91fa2a3f556198a7cbf26db32492a23e004a69
CougarChan <gohan0136@gmail.com>
2022-03-31 20:39:04 -0400

    Update

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+<<<<<<< HEAD
 router.get('/', (req, res) => {
     res.render('homepage', {
         id: 1,
@@ -11,5 +12,43 @@ router.get('/', (req, res) => {
         }
     });
 });
+=======
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models')
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // pass a single post object into the homepage template
+        res.render('homepage', dbPostData[0]);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+>>>>>>> German
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+<<<<<<< HEAD
 const homeRoutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+=======
+
+router.use('/api', apiRoutes);
+>>>>>>> German
 
 router.use((req, res) => {
     res.status(404).end();
