4f68af04fc72b4bc3eb141ed96dd29856ba4f20c
charon <cariasjosue@gmail.com>
2022-04-01 17:24:14 -0400

    dashbaord

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('homepage', {
+    res.render('homepage',  {
         id: 1,
         post_url: 'https://blahblog.com/first/',
-        title: 'My first blog',
+        title: 'Home Page',
+        created_at: new Date(),
+        user: {
+            username: 'random_user'
+        }
+    });
+});
+
+router.get('/homepage', (req, res) => {
+    res.render('homepage',  {
+        id: 1,
+        post_url: 'https://blahblog.com/first/',
+        title: 'Dashboard',
         created_at: new Date(),
         user: {
             username: 'random_user'
