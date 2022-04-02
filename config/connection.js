adfd6d76222facaaf73e39f135759107e79b07c5
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-04-01 17:31:53 -0700

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -18,12 +18,12 @@ require('dotenv').config();
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
     });
-
+    
 // if (process.env.JAWSDB_URL) {
 //   const sequelize = new Sequelize(process.env.JAWSDB_URL);
 // } else {
@@ -39,26 +39,7 @@ const sequelize = process.env.JAWSDB_URL
 //   );
 // } 
 
-// create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
+
     
 module.exports = sequelize;
 
-//JAWSDB
-/*
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
-*/
