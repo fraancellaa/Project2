d13140a09ed46faf0e152092614674c18c613739
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-04-01 22:06:55 -0700

    image and stylesheet changes

--- config/connection.js
+++ config/connection.js
@@ -15,14 +15,14 @@ require('dotenv').config();
 //   }
 // );
 
-// create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
+// // create connection to our db
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     });
     
 // if (process.env.JAWSDB_URL) {
 //   const sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -39,7 +39,22 @@ const sequelize = process.env.JAWSDB_URL
 //   );
 // } 
 
+let sequelize;
 
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
     
 module.exports = sequelize;
 
