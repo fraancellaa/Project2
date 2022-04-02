e52b939e3e0073e99f71208e724a22cfbb650340
Francella Aguilar <francellaaguilar@Francellas-MBP.attlocal.net>
2022-04-02 13:48:55 -0400

    js add, delete, edit

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,40 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection')
+const {Post, User } = require('../models');
 
-router.get('/', (req, res) => {
-    res.render('homepage', {
-        id: 1,
-        post_url: 'https://blahblog.com/first/',
-        title: 'My first blog',
-        created_at: new Date(),
-        user: {
-            username: 'random_user'
-        }
-    });
-});
+// get all posts for homepage
+// router.get('/', (req, res) => {
+//     Post.findAll({
+//         attributes: [
+//             'id',
+//             'post_url',
+//             'title',
+//             'created_at'
+//         ],
+//         include: [
+//             {
+//                 model: User,
+//                 attributes: ['username']
+//             }
+//         ]
+//     })
+//     .then(dbPostData => {
+//         const posts = dbPostData.map(post => post.get({ plain: true }));
 
+//         res.render('homepage', {
+//             posts,
+//             loggedIn: req.session.loggedIn
+//         });
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
+// });
+
+// get single post
+
+// login router
 router.get('/login', (req, res) => {
     res.render('login');
 })
