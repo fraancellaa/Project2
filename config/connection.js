1bf163eb907c87288e5ca44db7c011e984d6bfa3
CougarChan <gohan0136@gmail.com>
2022-04-04 13:26:04 -0400

    German

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+<<<<<<< .merge_file_a13136
 // const sequelize = new Sequelize(
 //   process.env.DB_NAME,
 //   process.env.DB_USER,
@@ -12,6 +13,23 @@ require('dotenv').config();
 //     port: 3306,
 //   }
 // );
+=======
+
+// if (process.env.JAWSDB_URL) {
+//   const sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306,
+//     },
+//   );
+// } 
+>>>>>>> .merge_file_a13584
 
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
@@ -21,5 +39,10 @@ const sequelize = process.env.JAWSDB_URL
       dialect: 'mysql',
       port: 3306
     });
+<<<<<<< .merge_file_a13136
 
 module.exports = sequelize;
+=======
+    
+module.exports = sequelize;
+>>>>>>> .merge_file_a13584
