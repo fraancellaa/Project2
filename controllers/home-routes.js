790d4205ba621c489bf3feeb2f769b1882e6a191
CougarChan <gohan0136@gmail.com>
2022-04-04 18:26:56 -0400

    update

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,99 +1,41 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection')
-const {Post, User } = require('../models');
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> Francella
 router.get('/', (req, res) => {
-    res.render('homepage', {
+    res.render('homepage',  {
         id: 1,
         post_url: 'https://blahblog.com/first/',
-        title: 'My first blog',
+        title: 'Home Page',
         created_at: new Date(),
         user: {
             username: 'random_user'
         }
     });
 });
-<<<<<<< HEAD
-=======
-=======
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models')
 
-router.get('/', (req, res) => {
-    Post.findAll({
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
-          model: User,
-          attributes: ['username']
+router.get('/homepage', (req, res) => {
+    res.render('homepage',  {
+        id: 1,
+        post_url: 'https://blahblog.com/first/',
+        title: 'Dashboard',
+        created_at: new Date(),
+        user: {
+            username: 'random_user'
         }
-      ]
-    })
-      .then(dbPostData => {
-        // pass a single post object into the homepage template
-        res.render('homepage', dbPostData[0]);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
->>>>>>> German
-=======
-// get all posts for homepage
-// router.get('/', (req, res) => {
-//     Post.findAll({
-//         attributes: [
-//             'id',
-//             'post_url',
-//             'title',
-//             'created_at'
-//         ],
-//         include: [
-//             {
-//                 model: User,
-//                 attributes: ['username']
-//             }
-//         ]
-//     })
-//     .then(dbPostData => {
-//         const posts = dbPostData.map(post => post.get({ plain: true }));
->>>>>>> e52b939e3e0073e99f71208e724a22cfbb650340
+    });
+});
 
-//         res.render('homepage', {
-//             posts,
-//             loggedIn: req.session.loggedIn
-//         });
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
-// });
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard',  {
+        id: 1,
+        post_url: 'https://blahblog.com/first/',
+        title: 'Dashboard',
+        created_at: new Date(),
+        user: {
+            username: 'random_user'
+        }
+    });
+});
 
-// get single post
->>>>>>> Francella
-
-// login router
 router.get('/login', (req, res) => {
     res.render('login');
 })
